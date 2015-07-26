b4d68bd729a0d37dd06c3f5d5cf26b1c970664d1
Adan Piñero Alquegui <adan@pinero.eu>
2015-07-26 11:56:35 +0200

    Soved firs-run preferences default.

--- app/src/main/java/com/example/android/popularmoviess100/MainActivity.java
+++ app/src/main/java/com/example/android/popularmoviess100/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.popularmoviess100;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PreferenceManager.setDefaultValues(this,R.xml.pref_general,true);
     }
 
 
